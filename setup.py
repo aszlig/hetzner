e62d6ed213aa6123fa9bb7942941e82ce4c878bb
aszlig <aszlig@redmoonstudios.org>
2012-06-06 16:35:05 +0200

    Bump version to 0.1.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name = 'hetzner',
-      version = '0.1.1',
+      version = '0.1.2',
       description = 'High level access to the Hetzner robot',
       url = 'https://github.com/RedMoonStudios/hetzner',
       author = 'aszlig',
