e36e5f88f4e8a7dbf1bb3aebae9c7785af105886
aszlig <aszlig@redmoonstudios.org>
2013-07-09 11:01:30 +0200

    Bump version number to 0.2.0.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 pythonPackages.buildPythonPackage rec {
   name = "hetzner-${version}";
-  version = "0.1.2";
+  version = "0.2.0";
   src = ./.;
   doCheck = false;
   installCommand = "python setup.py install --prefix=$out";
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='hetzner',
-      version='0.1.2',
+      version='0.2.0',
       description='High level access to the Hetzner robot',
       url='https://github.com/RedMoonStudios/hetzner',
       author='aszlig',
