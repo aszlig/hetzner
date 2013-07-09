9b468b245cdfe55cbb383d886a1dc54b1d0757f6
aszlig <aszlig@redmoonstudios.org>
2013-07-09 10:37:02 +0200

    Split up hetzner into hetzner.{robot,server}.
    This should bring better structure into the code and we don't end up with a big
    file with a lot of cross-references.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hetzner/server.py
+++ hetzner/server.py
@@ -1,21 +1,10 @@
 import os
 import time
-import json
 import socket
 import subprocess
 
 from tempfile import mkdtemp
 from datetime import datetime
-from functools import partial
-from base64 import b64encode
-from urllib import urlencode
-from httplib import HTTPSConnection, BadStatusLine
-
-ROBOT_HOST = "robot-ws.your-server.de"
-
-
-class RobotError(Exception):
-    pass
 
 
 class ManualReboot(Exception):
@@ -51,66 +40,6 @@ class SSHAskPassHelper(object):
             os.unlink(self.script)
         if self.tempdir is not None:
             os.rmdir(self.tempdir)
-
-
-class RobotConnection(object):
-    def __init__(self, user, passwd):
-        self.user = user
-        self.passwd = passwd
-        self.conn = HTTPSConnection(ROBOT_HOST)
-
-    def _request(self, method, path, data, headers, retry=1):
-        self.conn.request(method.upper(), path, data, headers)
-        try:
-            return self.conn.getresponse()
-        except BadStatusLine:
-            # XXX: Sometimes, the API server seems to have a problem with
-            # keepalives.
-            if retry <= 0:
-                raise
-
-            self.conn.close()
-            self.conn.connect()
-            return self._request(method, path, data, headers, retry - 1)
-
-    def request(self, method, path, data=None):
-        if data is not None:
-            data = urlencode(data)
-
-        auth = 'Basic {0}'.format(
-            b64encode("{0}:{1}".format(self.user, self.passwd))
-        )
-
-        headers = {'Authorization': auth}
-
-        if data is not None:
-            headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-        response = self._request(method, path, data, headers)
-        data = json.loads(response.read())
-
-        if 200 <= response.status < 300:
-            return data
-        else:
-            error = data.get('error', None)
-            if error is None:
-                raise RobotError("Unknown error: {0}".format(data))
-            else:
-                err = "{0} - {1}".format(error['status'], error['message'])
-                if 'missing' in error:
-                    err += ", missing input: {0}".format(
-                        ', '.join(error['missing'])
-                    )
-                if 'invalid' in error:
-                    err += ", invalid input: {0}".format(
-                        ', '.join(error['invalid'])
-                    )
-                raise RobotError(err)
-
-    get = lambda s, p: s.request('GET', p)
-    post = lambda s, p, d: s.request('POST', p, d)
-    put = lambda s, p, d: s.request('PUT', p, d)
-    delete = lambda s, p, d: s.request('DELETE', p, d)
 
 
 class RescueSystem(object):
@@ -303,26 +232,3 @@ class Server(object):
 
     def __repr__(self):
         return "<{0} ({1})>".format(self.ip, self.product)
-
-
-class ServerManager(object):
-    def __init__(self, conn):
-        self.conn = conn
-
-    def __getitem__(self, ip):
-        """
-        Get server by providing its main IP address.
-
-        For example:
-        robot.servers['1.2.3.4']
-        """
-        return Server(self.conn, self.conn.get('/server/{0}'.format(ip)))
-
-    def __iter__(self):
-        return iter([Server(self.conn, s) for s in self.conn.get('/server')])
-
-
-class Robot(object):
-    def __init__(self, user, passwd):
-        self.conn = RobotConnection(user, passwd)
-        self.servers = ServerManager(self.conn)
