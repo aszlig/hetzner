ff1e3c3821d98a627275019aba6037686efcf012
aszlig <aszlig@redmoonstudios.org>
2013-07-09 11:25:05 +0200

    Bump version number to 0.2.1.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 pythonPackages.buildPythonPackage rec {
   name = "hetzner-${version}";
-  version = "0.2.0";
+  version = "0.2.1";
   src = ./.;
   doCheck = false;
   installCommand = "python setup.py install --prefix=$out";
