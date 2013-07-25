bc02f1f4aa762ccc30b98a1f237b99a2a0656775
aszlig <aszlig@redmoonstudios.org>
2013-07-25 16:32:01 +0200

    Bump version to 0.5.0.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='hetzner',
-      version='0.4.1',
+      version='0.5.0',
       description='High level access to the Hetzner robot',
       url='https://github.com/RedMoonStudios/hetzner',
       author='aszlig',
