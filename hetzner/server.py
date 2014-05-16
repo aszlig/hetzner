453ff81c7205451df0a2c7103e7b7d3509d414a0
Luar Roji <luar@scrapinghub.com>
2014-05-16 03:58:35 -0400

    Added support for RAC rebooting of DELL servers.

--- hetzner/server.py
+++ hetzner/server.py
@@ -432,9 +432,9 @@ class Server(object):
 
         if tries is None:
             if self.is_dell:
-                tries = ['ssh']
+                tries = ['ssh', 'rac']
 
-                # These servers take more than 5 minutes to reboot
+                # These servers take more than 5 minutes to reboot.
                 if patience <= 300:
                     patience = 600
             elif self.is_vserver:
@@ -481,12 +481,28 @@ class Server(object):
         """
 
         if self.is_dell:
+            if mode == "ssh":
+                # XXX: Warning, this only works for linux/unix systems.
+
+                try:
+                    self.shell(None, cmdline="reboot")
+                except Exception, ex:
+                    print ex
+
+                return True
+            elif mode == "rac":
+                try:
+                    ips = list(self.ips)
+                    ip = ips[1].ip
+                except:
+                    ip = ""
+
+                rac = RAC(ip)
+                rac.powercycle()
+
+                return True
             if mode != "ssh":
                 raise RobotError("DELL servers don't allow reboot via robot.")
-
-            # XXX: Warning, this only works for linux/unix systems.
-            self.shell(None, cmdline="reboot")
-            return True
 
         if self.is_vserver:
             if mode == 'soft':
@@ -533,3 +549,64 @@ class Server(object):
 
     def __repr__(self):
         return "<{0} (#{1} {2})>".format(self.ip, self.number, self.product)
+
+
+# Code got from https://github.com/migrantgeek/python-rac
+
+class RAC(object):
+
+    def __init__(self, host):
+        import getpass
+        self.sid = None
+
+        self.host = raw_input("IP of the iDRAC console [{0}]:".format(host)) or host
+        self.username = raw_input("Username [root]:") or "root"
+        self.password = getpass.getpass("Password:")
+
+    def _inject_header(self, data):
+        if data is not None:
+            return "<?xml version='1.0'?>" + data
+
+    def _extract_value(self, data, value):
+        if data is None:
+            return
+        try:
+            return data.split('<%s>' % value)[1].split('</%s>' % value)[0]
+        except KeyError:
+            raise Exception('unable to extract %s' % value)
+
+    def _extract_sid(self, data):
+        return self._extract_value(data, 'SID')
+
+    def _extract_cmd_output(self, data):
+        return self._extract_value(data, 'CMDOUTPUT')
+
+    def _make_request(self, uri, data=None):
+        import urllib2
+
+        opener = urllib2.build_opener()
+        if self.sid:
+            opener.addheaders.append(('Cookie', 'sid=%s' % self.sid))
+        return opener.open('https://%s/cgi-bin/%s' % (self.host, uri),
+                self._inject_header(data)).read()
+
+    def _login(self):
+        data = '<LOGIN><REQ><USERNAME>%s</USERNAME><PASSWORD>%s</PASSWORD></REQ></LOGIN>' % (self.username, self.password)
+        resp = self._make_request('/login', data)
+        self.sid = self._extract_sid(resp)
+
+    def _logout(self):
+        self.sid = None
+        self._make_request('/logout')
+
+    def run_command(self, cmd):
+        if self.sid is None:
+            self._login()
+        try:
+            data = '<EXEC><REQ><CMDINPUT>racadm %s</CMDINPUT><MAXOUTPUTLEN>0x0fff</MAXOUTPUTLEN></REQ></EXEC>' % cmd
+            return self._extract_cmd_output(self._make_request('/exec', data)).strip()
+        finally:
+            self._logout()
+
+    def powercycle(self):
+        return self.run_command('serveraction powercycle')
