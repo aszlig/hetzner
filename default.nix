328b440e68f149020c650a6f71a8a140dc1f8cad
aszlig <aszlig@redmoonstudios.org>
2014-09-11 13:56:48 +0200

    Bump version to 0.7.2.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 pythonPackages.buildPythonPackage rec {
   name = "hetzner-${version}";
-  version = "0.7.1";
+  version = "0.7.2";
   src = ./.;
   doCheck = false;
   installCommand = "python setup.py install --prefix=$out";
