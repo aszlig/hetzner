46c6574bedbcf85c4fdfa3b1eea1df4c1a4a58c2
aszlig <aszlig@redmoonstudios.org>
2016-12-07 23:54:08 +0100

    default.nix: Clean up
    We do have a few unit tests now and we also don't need that
    installCommand workaround anymore (not sure why I used it in the first
    place though).
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -4,6 +4,4 @@ pythonPackages.buildPythonPackage rec {
   name = "hetzner-${version}";
   version = "0.7.1";
   src = ./.;
-  doCheck = false;
-  installCommand = "python setup.py install --prefix=$out";
 }
