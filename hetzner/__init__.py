aszlig/hetzner

commit 788b3fd2cdfa8cae360bd2553ca838096efc30a8

RobotError: Don't call __init__() supermethod
First of all, the change introduced in
330ce550f4d31a7a43625537e414e4e9794a8043 is not PEP8-compliant (too long
line) and also I think it's a bit odd to call the __init__() method of
Exception *without* giving it a status attribute and then setting it in
our own __init__().

Setting self.message and self.status directly is also documented in the
official Python documentation, so we don't rely on internals here as
well, see:

https://docs.python.org/3/tutorial/errors.html#user-defined-exceptions

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

hetzner/__init__.py
@@ -1,7 +1,9 @@
 class RobotError(Exception):
     def __init__(self, message, status=None):
-        formattedMessage = message if status is None else "{0} ({1})".format(message, status)
-        super(Exception, self).__init__(formattedMessage)
+        if status is None:
+            self.message = message
+        else:
+            self.message = "{0} ({1})".format(message, status)
         self.status = status
 
 

commit c7181fd0763a08754a3305da82e1bb649827f4a1

RobotError: Correctly initialize exception message
Regression introduced by 788b3fd2cdfa8cae360bd2553ca838096efc30a8.

While setting self.message is documented for setting user-defined
exceptions, it might be a misunderstanding by me and it's *not* about
the printed result of the exception but about setting custom attributes
for an exception.

After digging a bit deeper on this, I found that both Python 2.7 and 3.x
simply accumulate all arguments passed to __init__() to self.args, see:

https://github.com/python/cpython/blob/2.7/Objects/exceptions.c#L65
https://github.com/python/cpython/blob/3.6/Objects/exceptions.c#L66

Testing this with the following example leads to the expected result:

    class Foo(Exception):
        def __init__(self):
            super(Foo, self).__init__(111, "bar")

    raise Foo()

Result for Python 2.7.13:

    Traceback (most recent call last):
      File "exctest.py", line 5, in <module>
        raise Foo()
    __main__.Foo: (111, 'bar')

Result for Python 3.6.1:

    Traceback (most recent call last):
      File "exctest.py", line 5, in <module>
        raise Foo()
    __main__.Foo: (111, 'bar')

So the self.args attribute is just passed to repr() and/or str() and
that's about it.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Issue: #23
Cc: @nh2

hetzner/__init__.py
@@ -1,10 +1,8 @@
 class RobotError(Exception):
     def __init__(self, message, status=None):
-        if status is None:
-            self.message = message
-        else:
-            self.message = "{0} ({1})".format(message, status)
-        self.status = status
+        if status is not None:
+            message = "{0} ({1})".format(message, status)
+        super(RobotError, self).__init__(message)
 
 
 class ManualReboot(Exception):