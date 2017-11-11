df10164f1fd7b7db0d8e38a9cd6f258360e122df
Peter Klein (Let's make sense GmbH) <p.klein@letsmakesense.de>
2017-11-11 17:04:38 +0100

    Add options to list/set failover addresses

---
+++ hetzner/failover.py
@@ -0,0 +1,55 @@
+from hetzner import RobotError
+
+__all__ = ['Failover', 'FailoverManager']
+
+
+class Failover(object):
+    ip = None
+    server_ip = None
+    server_number = None
+    active_server_ip = None
+
+    def __repr__(self):
+        return "%s (destination: %s, booked on %s (%s))" % (
+            self.ip, self.active_server_ip, self.server_number, self.server_ip)
+
+    def __init__(self, data):
+        for attr, value in data.items():
+            if hasattr(self, attr):
+                setattr(self, attr, value)
+
+
+class FailoverManager(object):
+
+    def __init__(self, conn, servers):
+        self.conn = conn
+        self.servers = servers
+
+    def list(self):
+        failovers = {}
+        ips = self.conn.get('/failover')
+        for ip in ips:
+            failover = Failover(ip.get('failover'))
+            failovers[failover.ip] = failover
+        return failovers
+
+    def set(self, ip, new_destination):
+        failovers = self.list()
+        if ip not in failovers.keys():
+            raise RobotError(
+                "Invalid IP address '%s'. Failover IP addresses are %s"
+                % (ip, failovers.keys()))
+        failover = failovers.get(ip)
+        if new_destination == failover.active_server_ip:
+            raise RobotError(
+                "%s is already the active destination of failover IP %s"
+                % (new_destination, ip))
+        available_dests = [s.ip for s in list(self.servers)]
+        if new_destination not in available_dests:
+            raise RobotError(
+                "Invalid destination '%s'. "
+                "The destination is not in your server list: %s"
+                % (new_destination, available_dests))
+        result = self.conn.post('/failover/%s'
+                                % ip, {'active_server_ip': new_destination})
+        return Failover(result.get('failover'))
