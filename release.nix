3ffeb29521cc72bcfe67e60995248d34f82d705d
aszlig <aszlig@nix.build>
2017-11-18 13:35:25 +0100

    release.nix: Automatically determine Python sets
    We have different Python versions for different nixpkgs releases, for
    example with release 17.09 there no longer is a set for Python 3.3, so
    let's actually determine the available package sets by matching on the
    attribute names in the top-level package set.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- release.nix
+++ release.nix
@@ -1,22 +1,29 @@
 { nixpkgs ? <nixpkgs>
-, systems ? [
-    "armv6l-linux" "armv7l-linux" "i686-linux" "x86_64-linux"
-  ]
-, pythonVersions ? [
-    "python27" "python33" "python34" "python35" "python36" "pypy"
-  ]
+, systems ? [ "armv6l-linux" "armv7l-linux" "i686-linux" "x86_64-linux" ]
 }:
 
 let
   inherit (import nixpkgs {}) lib;
 
-  mkPackageSet = system: pyver: let
-    pkgs = import nixpkgs { inherit system; };
-    pythonPackages = pkgs."${pyver}Packages";
-  in {
-    hetzner = import ./. { inherit pythonPackages; };
-  };
+  getPythonName = name: let
+    match = builtins.match "python([0-9]{2,})Packages" name;
+    cpython = if match != null then "python${lib.head match}" else null;
+  in if name == "pypyPackages" then "pypy" else cpython;
 
-  mkSystems = system: lib.genAttrs pythonVersions (mkPackageSet system);
+  getPythonSetsFor = system: let
+    pkgs = import nixpkgs { inherit system; };
+    getPythonSet = name: acc: let
+      pyname = getPythonName name;
+      newAcc = acc // { ${pyname} = pkgs.${name}; };
+    in if pyname == null then acc else newAcc;
+  in lib.fold getPythonSet {} (lib.attrNames pkgs);
+
+  mkPackageSet = system: pyver: pythonPackages: let
+    pkgs = import nixpkgs { inherit system; };
+  in { hetzner = import ./. { inherit pythonPackages; }; };
+
+  mkSystems = system: let
+    pythonSets = getPythonSetsFor system;
+  in lib.mapAttrs (mkPackageSet system) pythonSets;
 
 in lib.genAttrs systems mkSystems
