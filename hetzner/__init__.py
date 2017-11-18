aszlig/hetzner

commit c7181fd0763a08754a3305da82e1bb649827f4a1

RobotError: Correctly initialize exception message
Regression introduced by 788b3fd2cdfa8cae360bd2553ca838096efc30a8.

While setting self.message is documented for setting user-defined
exceptions, it might be a misunderstanding by me and it's *not* about
the printed result of the exception but about setting custom attributes
for an exception.

After digging a bit deeper on this, I found that both Python 2.7 and 3.x
simply accumulate all arguments passed to __init__() to self.args, see:

https://github.com/python/cpython/blob/2.7/Objects/exceptions.c#L65
https://github.com/python/cpython/blob/3.6/Objects/exceptions.c#L66

Testing this with the following example leads to the expected result:

    class Foo(Exception):
        def __init__(self):
            super(Foo, self).__init__(111, "bar")

    raise Foo()

Result for Python 2.7.13:

    Traceback (most recent call last):
      File "exctest.py", line 5, in <module>
        raise Foo()
    __main__.Foo: (111, 'bar')

Result for Python 3.6.1:

    Traceback (most recent call last):
      File "exctest.py", line 5, in <module>
        raise Foo()
    __main__.Foo: (111, 'bar')

So the self.args attribute is just passed to repr() and/or str() and
that's about it.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>
Issue: #23
Cc: @nh2

hetzner/__init__.py
@@ -1,10 +1,8 @@
 class RobotError(Exception):
     def __init__(self, message, status=None):
-        if status is None:
-            self.message = message
-        else:
-            self.message = "{0} ({1})".format(message, status)
-        self.status = status
+        if status is not None:
+            message = "{0} ({1})".format(message, status)
+        super(RobotError, self).__init__(message)
 
 
 class ManualReboot(Exception):

commit 6f1a9f8b5b373b3408dda5a8da90c4ee941c3d1d

RobotError: Assign status to the instance
Regression introduced by c7181fd0763a08754a3305da82e1bb649827f4a1.

The mentioned commit accidentally removed the status attribute, which we
currently need to check for the HTTP status code in some requests.

Signed-off-by: aszlig <aszlig@nix.build>

hetzner/__init__.py
@@ -3,6 +3,7 @@ class RobotError(Exception):
         if status is not None:
             message = "{0} ({1})".format(message, status)
         super(RobotError, self).__init__(message)
+        self.status = status
 
 
 class ManualReboot(Exception):