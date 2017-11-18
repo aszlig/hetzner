a54c3bc1010007f95c379ce76fd6484e73af1f6a
Peter Klein (Let's make sense GmbH) <p.klein@letsmakesense.de>
2017-11-18 12:39:09 +0100

    - removed code style violations

--- hetzner/failover.py
+++ hetzner/failover.py
@@ -2,6 +2,7 @@ from hetzner import RobotError
 
 __all__ = ['Failover', 'FailoverManager']
 
+
 class Failover(object):
     ip = None
     server_ip = None
@@ -35,17 +36,20 @@ class FailoverManager(object):
     def set(self, ip, new_destination):
         failovers = self.list()
         if ip not in failovers.keys():
-            raise RobotError("Invalid IP address '%s'. Failover IP addresses are %s" % (ip, failovers.keys()))
+            raise RobotError(
+                "Invalid IP address '%s'. Failover IP addresses are %s"
+                % (ip, failovers.keys()))
         failover = failovers.get(ip)
         if new_destination == failover.active_server_ip:
-            raise RobotError("%s is already the active destination of failover IP %s" % (new_destination, ip))
+            raise RobotError(
+                "%s is already the active destination of failover IP %s"
+                % (new_destination, ip))
         available_dests = [s.ip for s in list(self.servers)]
         if new_destination not in available_dests:
-            raise RobotError("Invalid destination '%s'. The destination is not in your server list: %s" %
-                             (new_destination, available_dests))
-        result = self.conn.post('/failover/%s' % ip, {'active_server_ip': new_destination})
+            raise RobotError(
+                "Invalid destination '%s'. "
+                "The destination is not in your server list: %s"
+                % (new_destination, available_dests))
+        result = self.conn.post('/failover/%s'
+                                % ip, {'active_server_ip': new_destination})
         return Failover(result.get('failover'))
-
-
-
-
