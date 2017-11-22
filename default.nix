dc5527da9396c1fb30a9368cf5f0811e7123273e
aszlig <aszlig@nix.build>
2017-11-22 15:15:33 +0100

    default.nix: Filter src attribute
    We can't use lib.cleanSource here, because we need to exclude a few
    Python-specific files.
    
    This should ensure that we don't constantly rebuild with a different
    result link (because the latter is included into the store and thus
    changes the build input).
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- default.nix
+++ default.nix
@@ -5,5 +5,17 @@ pythonPackages.buildPythonPackage rec {
   version = let
     matchVersion = builtins.match ".*version=[\"']([^\"']+)[\"'].*";
   in builtins.head (matchVersion (builtins.readFile ./setup.py));
-  src = ./.;
+
+  src = let
+    filter = path: type: let
+      name = baseNameOf (toString path);
+      dirCond = name == ".git" || name == "build" || name == "dist";
+      linkCond = builtins.match "result.*" name != null;
+      cond = builtins.match ".*\\.py[oc]$" name != null
+          || builtins.match ".*\\.sw[a-z]$" name != null
+          || name == "MANIFEST";
+    in if type == "directory" then !dirCond
+       else if type == "symlink" then !linkCond
+       else !cond;
+  in builtins.filterSource filter ./.;
 }
