aszlig/hetzner

commit 4f20a07e738802eda2fe9725b3a286d299721e1a

server: Fix a few PEP8 errors
Apart from a few minor useless spaces before the authorized_keys
property, the from_data argument actually would have triggered a runtime
exception, so I fixed that as well.

Signed-off-by: aszlig <aszlig@nix.build>
Cc: @farcaller

hetzner/server.py
@@ -61,10 +61,10 @@ class RescueSystem(object):
         self._password = None
         self._authorized_keys = None
 
-    def _update_status(self, from_data=None):
-        if not from_data:
-            from_data = self.conn.get('/boot/{0}/rescue'.format(self.server.ip))
-        data = reply['rescue']
+    def _update_status(self, data=None):
+        if data is None:
+            path = '/boot/{0}/rescue'.format(self.server.ip)
+            data = self.conn.get(path)
         self._active = data['active']
         self._password = data['password']
         self._authorized_keys = data['authorized_key']
@@ -82,7 +82,7 @@ class RescueSystem(object):
             return self._password
         self._update_status()
         return self._password
-    
+
     @property
     def authorized_keys(self):
         if self._authorized_keys is not None:

commit 3db7fddf7c1b010183b386bff9eb9593aa073b40

server: Fix fetching rescue system information
Regression introduced by 4f20a07e738802eda2fe9725b3a286d299721e1a.

I accidentally removed accessing the "rescue" subobject from the
response, which subsequently will fail with an error when status
information about the rescue system is gathered.

Signed-off-by: aszlig <aszlig@nix.build>

hetzner/server.py
@@ -65,9 +65,10 @@ class RescueSystem(object):
         if data is None:
             path = '/boot/{0}/rescue'.format(self.server.ip)
             data = self.conn.get(path)
-        self._active = data['active']
-        self._password = data['password']
-        self._authorized_keys = data['authorized_key']
+        rescue = data['rescue']
+        self._active = rescue['active']
+        self._password = rescue['password']
+        self._authorized_keys = rescue['authorized_key']
 
     @property
     def active(self):