d3d7af65098ee177fe86c47ac7ab62cdf061e8ab
aszlig <aszlig@nix.build>
2019-05-27 19:10:15 +0200

    Bump version to 0.8.2
    Signed-off-by: aszlig <aszlig@nix.build>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class RunTests(Command):
 
 
 setup(name='hetzner',
-      version='0.8.1',
+      version='0.8.2',
       description='High level access to the Hetzner robot',
       url='https://github.com/aszlig/hetzner',
       author='aszlig',
