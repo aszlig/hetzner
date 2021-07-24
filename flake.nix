cfca593159eb352485ef485f10de8b97a55de1b3
aszlig <aszlig@nix.build>
2021-07-24 14:36:17 +0200

    Switch to basic Nix Flake
    The implementation is pretty much equivalent to what we have so far in
    default.nix and release.nix, but the advantage of using a Flake is that
    we can pin a nixpkgs version and also provide Hydra with all the
    required parameters so we no longer need to provide external settings.
    
    This not only makes the tests more reproducible but also helps against
    amnesia, eg. when you forget the correct function attrs for release.nix.
    
    Signed-off-by: aszlig <aszlig@nix.build>

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "High level access to the Hetzner robot";
+
+  outputs = { self, nixpkgs }: let
+    inherit (nixpkgs) lib;
+
+    hydraSystems = [ "i686-linux" "x86_64-linux" ];
+
+    mkPackage = pythonPackages: pythonPackages.buildPythonPackage {
+      pname = "hetzner";
+
+      version = let
+        matchVersion = builtins.match ".*version=[\"']([^\"']+)[\"'].*";
+      in builtins.head (matchVersion (builtins.readFile ./setup.py));
+
+      src = self;
+    };
+
+  in {
+    packages = lib.mapAttrs (lib.const (pkgs: {
+      hetzner = mkPackage pkgs.python3Packages;
+    })) nixpkgs.legacyPackages;
+
+    defaultPackage = let
+      getPackage = system: lib.const (self.packages.${system}.hetzner);
+    in lib.mapAttrs getPackage nixpkgs.legacyPackages;
+
+    checks = lib.mapAttrs (system: pkgs: let
+      interpreters = removeAttrs pkgs.pythonInterpreters [
+        # These fail to eveluate or build (the latter because we require TLS).
+        "graalpython37" "python3Minimal"
+      ];
+      isEligible = lib.const (interpreter: let
+        rightPlatform = lib.elem system interpreter.meta.platforms;
+        rightVersion = interpreter.pythonAtLeast "3.7";
+      in interpreter ? pythonAtLeast && rightVersion && rightPlatform);
+      supported = lib.filterAttrs isEligible interpreters;
+      mkInterpreterPackage = lib.const (interpreter: {
+        name = lib.removeSuffix "3" interpreter.pname
+             + "-${interpreter.pythonVersion}";
+        value = mkPackage interpreter.pkgs;
+      });
+    in lib.mapAttrs' mkInterpreterPackage supported) nixpkgs.legacyPackages;
+
+    hydraJobs = lib.genAttrs hydraSystems (lib.flip lib.getAttr self.checks);
+  };
+}
